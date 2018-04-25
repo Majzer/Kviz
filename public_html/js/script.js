b1c1d63c4d92437a8c4e67e47bbe612dfa5fcb6c
Majzer <bence@mozer.hu>
2018-04-25 19:54:29 +0200

    Jobb oldal, újra

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -3,14 +3,27 @@ function kiir(szoveg) {
     kerdes[0].innerHTML = szoveg;
 }
 
-var kerdesek = [["Hogyan keződik? \"... a gőzmozdony\"", 2, "Persi", "Thomas", "Bob", "Chuggington"], ["Mit szeret Micimackó a leginkább?", 1, "Méz", "Eper", "Csoki", "Tejszínhab"], ["Mennyi a következő egyenlet megoldása: 2*(3+4)/7", 4, "3.3", "1", "7", "2"], ["Hogy hívtak majdnem mindenkit a Rrrrrrrrr c. filmben?", 2, "Béla", "Pali", "Szőke", "Jani"], ["A sárga és a kék szín keverésével milyen színt kapunk?", 1, "Zöld", "Piros", "Kék", "Lila"], ["Milyen kategóriájú jogosítvánnyal lehet személygépkocsit vezetni?", 2, "A", "B", "C", "D"], ["Melyik képzeletbeli város Batman otthona?", 4, "New York", "Budapest", "Joker", "Gotham"], ["Hány csoda van a világon?", 3, "1", "3", "7", "12"], ["Milyen magas a Mt. Everest?", 1, "8848 m", "9659 m", "13027 m", "15984 m"], ["Milyen színű a kör a japán nemzeti zászlóban?", 1, "Piros", "Kék", "Sárga", "Zöld"]];
+var kerdesek = [["Hogyan keződik? \"... a gőzmozdony\"", 2, "Persi", "Thomas", "Bob", "Chuggington"], ["Mit szeret Micimackó a leginkább?", 1, "Méz", "Eper", "Csoki", "Tejszínhab"], ["Mennyi a következő egyenlet megoldása: 2*(3+4)/7", 4, "3.3", "1", "7", "2"], ["Hogy hívtak majdnem mindenkit a RRRrrrr!!! c. filmben?", 2, "Béla", "Pali", "Szőke", "Jani"], ["A sárga és a kék szín keverésével milyen színt kapunk?", 1, "Zöld", "Piros", "Kék", "Lila"], ["Milyen kategóriájú jogosítvánnyal lehet személygépkocsit vezetni?", 2, "A", "B", "C", "D"], ["Melyik képzeletbeli város Batman otthona?", 4, "New York", "Budapest", "Joker", "Gotham"], ["Hány csoda van a világon?", 3, "1", "3", "7", "12"], ["Milyen magas a Mt. Everest?", 1, "8848 m", "9659 m", "13027 m", "15984 m"], ["Milyen színű a kör a japán nemzeti zászlóban?", 1, "Piros", "Kék", "Sárga", "Zöld"]];
 var jovalasz;
 var vanvalasz = false;
 var jogomb;
 var rosszvalasz;
 var nyomott;
+var kerdesSzama = 0;
+var helyzetek;
 
 function init() {
+
+
+    var helyzetBlokk = document.getElementById("helyzetBlokk");
+    for (var i = 0; i < 10; i++) {
+        helyzetBlokk.innerHTML += "<div class='test'>" + (i + 1) + ". kérdés</div>";
+    }
+    betoltes();
+
+}
+
+function betoltes() {
     var tomb = kerdesek[(Math.floor(Math.random() * (9)))];
     var kerdes = tomb[0];
     kiir(kerdes);
@@ -19,7 +32,13 @@ function init() {
     var lehetosegek = document.getElementsByClassName("valasz");
     for (var i = 0; i < 4; i++) {
         lehetosegek[i].innerHTML = tomb[i + 2];
+        lehetosegek[i].style.backgroundColor = "#C0C0C0";
     }
+
+    helyzetek = document.getElementsByClassName("test");
+
+    helyzetek[kerdesSzama].style.backgroundColor = "yellow";
+    helyzetek[kerdesSzama].style.color = "black";
 }
 
 function ellenorzes(gomb) {
@@ -61,9 +80,25 @@ function ellenorzes(gomb) {
 function johet() {
     var nyomottgomb = document.getElementById(jogomb);
     nyomottgomb.style.backgroundColor = "green";
+    if (nyomott === jogomb) {
+        helyzetek[kerdesSzama++].style.backgroundColor = "green";
+        setTimeout(betoltes, 2000);
+    }
+
 }
 
-function piros(){
+function piros() {
     var nyomottgomb = document.getElementById(nyomott);
     nyomottgomb.style.backgroundColor = "red";
+    helyzetek[kerdesSzama++].style.backgroundColor = "red";
+    setTimeout(rossz, 2000);
+}
+
+function rossz() {
+    var valaszok = document.getElementById("valaszok");
+    valaszok.innerHTML = "<div id='ujra' onclick='ujra();'>Újra</div>";
+}
+
+function ujra() {
+    window.location.reload(false);
 }
